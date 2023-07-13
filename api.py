49e337ffe1ec128c62a9e25b082ca0f04271fd12
Ishita Sharma <113188038+IshitaxSharma@users.noreply.github.com>
2023-07-13 10:01:09 +0530

    api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from typing import List
 
 app = FastAPI()
 
-# Create a model for the mail data
+# Model for the mail data
 class Mail(BaseModel):
     sender: str
     recipients: List[str]
@@ -14,12 +14,10 @@ class Mail(BaseModel):
 # Define a route to send mail
 @app.post("/send-mail/")
 def send_mail(mail: Mail):
-    # Your code to send the mail goes here
-    # You can use libraries like smtplib or third-party APIs
+    # 
     
     return {"message": "Mail sent successfully"}
 
-# Run the API server with uvicorn
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="localhost", port=8000)
